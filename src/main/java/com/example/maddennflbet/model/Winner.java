cbb12be364bc55f9579de6ef322f89f4d2e9c7d8
Kevin <kbrown2derlon@gmail.com>
2022-02-02 09:04:32 -0600

    4th

--- src/main/java/com/example/maddennflbet/model/Winner.java
+++ src/main/java/com/example/maddennflbet/model/Winner.java
@@ -1,18 +1,39 @@
 package com.example.maddennflbet.model;
 
 
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table(name = )
-//public class Winner {
-//
-//
-//
-//    @Id
-//    @Column
-//
-//}
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "winners" )
+public class Winner {
+
+
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Integer bet;
+
+    @Column
+    private Integer winner;
+
+
+    public MaddenNFL getMaddenNfl() {
+        return maddenNfl;
+    }
+
+    public void setMaddenNfl(MaddenNfl maddenNfl){
+        this.maddenNfl = maddenNfl;
+    }
+
+    @ManyToOne
+    @JoinColumn(name =)
+    @JsonIgnore
+    private
+
+}
---
+++ src/main/java/com/example/maddennflbet/service/WinnerService.java
@@ -0,0 +1,4 @@
+package com.example.maddennflbet.service;
+
+public class WinnerService {
+}
