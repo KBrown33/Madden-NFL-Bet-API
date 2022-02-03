b99752a0ab6e6232f4d1474d326cb9bdc8e30216
Kevin <kbrown2derlon@gmail.com>
2022-02-03 09:20:39 -0600

    9th

--- src/main/java/com/example/maddennflbet/repository/MaddenNFLRepository.java
+++ src/main/java/com/example/maddennflbet/repository/MaddenNFLRepository.java
@@ -1,5 +1,6 @@
 package com.example.maddennflbet.repository;
 
+import com.example.maddennflbet.model.MaddenNFL;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MaddenNFLRepository extends JpaRepository<MaddenNFL, Long> {
--- src/main/java/com/example/maddennflbet/repository/TeamRepository.java
+++ src/main/java/com/example/maddennflbet/repository/TeamRepository.java
@@ -1,10 +1,13 @@
 package com.example.maddennflbet.repository;
 
 
+import com.example.maddennflbet.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
-    List<Team> findBy
+    List<Team> findByUser_Id(Long teamId);
 }
